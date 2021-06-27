efd8099aea584fea1ac3039c02306332bb946dcd
anatawa12 <anatawa12@icloud.com>
2021-06-27 18:50:34 +0900

    helper/gradle-wrapper: pass --stacktrace if --debug mode

--- src/helpers/gradle-wrapper.rs
+++ src/helpers/gradle-wrapper.rs
@@ -1,5 +1,5 @@
-use std::ffi::OsStr;
-use std::path::PathBuf;
+use std::ffi::{OsStr, OsString};
+use std::path::{Path, PathBuf};
 use std::process::Stdio;
 
 use tokio::io;
@@ -9,11 +9,25 @@ use super::*;
 
 pub struct GradleWrapperHelper {
     cwd: PathBuf,
+    options: Vec<OsString>,
 }
 
 impl GradleWrapperHelper {
     pub fn new<P: Into<PathBuf>>(cwd: P) -> Self {
-        Self { cwd: cwd.into() }
+        let mut options = vec![];
+        if log::log_enabled!(log::Level::Trace) {
+            options.push("--stacktrace".into());
+        }
+        Self {
+            cwd: cwd.into(),
+            options,
+        }
+    }
+
+    pub fn add_init_script(&mut self, path: impl AsRef<Path>) -> &mut Self {
+        self.options.push("--init-script".into());
+        self.options.push(path.as_ref().as_os_str().to_owned());
+        self
     }
 
     pub async fn run_tasks(
@@ -22,6 +36,7 @@ impl GradleWrapperHelper {
     ) -> io::Result<()> {
         run_err(
             Command::new(self.cwd.join("gradlew"))
+                .args(&self.options)
                 .arg("--")
                 .args(tasks)
                 .stdin(Stdio::null())

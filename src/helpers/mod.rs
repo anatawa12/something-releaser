8cf7f11c29e3d13bed2c473c28d7128a6eed1d42
anatawa12 <anatawa12@icloud.com>
2021-06-27 18:51:24 +0900

    helper: print command to run

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -5,6 +5,8 @@ pub use git::GitHelper;
 pub use gradle_wrapper::GradleWrapperHelper;
 pub use properties::PropertiesFile;
 
+use crate::*;
+
 mod git;
 #[path = "gradle-wrapper.rs"]
 mod gradle_wrapper;
@@ -13,6 +15,7 @@ mod properties;
 // helper utils
 
 async fn run_err(cmd: &mut Command) -> io::Result<()> {
+    trace!("run: {:?}", cmd);
     let exit_status = cmd.spawn()?.wait().await?;
     if !exit_status.success() {
         return Err(io::Error::new(

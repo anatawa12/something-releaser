8402259ba11081ad322bb113ff6149acaea437e0
anatawa12 <anatawa12@icloud.com>
2021-07-01 20:17:35 +0900

    remove gradle wrpper helper

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,25 +1,3 @@
-use tokio::io;
-use tokio::process::Command;
-
-pub use gradle_wrapper::GradleWrapperHelper;
 pub use properties::PropertiesFile;
 
-use crate::*;
-
-#[path = "gradle-wrapper.rs"]
-mod gradle_wrapper;
 mod properties;
-
-// helper utils
-
-async fn run_err(cmd: &mut Command) -> io::Result<()> {
-    trace!("run: {:?}", cmd);
-    let exit_status = cmd.spawn()?.wait().await?;
-    if !exit_status.success() {
-        return Err(io::Error::new(
-            io::ErrorKind::Other,
-            format!("process exited with non-zero value"),
-        ));
-    }
-    Ok(())
-}

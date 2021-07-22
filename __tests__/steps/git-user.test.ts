8062a377ba8f89de47ff2e18bdbeb1d08c3d5b80
anatawa12 <anatawa12@icloud.com>
2021-07-22 20:16:22 +0900

    apply eslint for tests

--- __tests__/steps/git-user.test.ts
+++ __tests__/steps/git-user.test.ts
@@ -5,23 +5,22 @@ import {expect, test} from '@jest/globals'
 const octokit = new github.GitHub()
 
 test('throws organization', async () => {
-  await expect(gitUser.findGitUser('fixrtm', octokit)).rejects.toThrow(
-    "You can't commit as a Organization"
-  )
+  await expect(gitUser.findGitUser('fixrtm', octokit))
+    .rejects.toThrow("You can't commit as a Organization")
 })
 
 test('user account', async () => {
-  await expect(gitUser.findGitUser('anatawa12', octokit)).resolves.toEqual([
-    'anatawa12',
-    '22656849+anatawa12@users.noreply.github.com'
-  ])
+  await expect(gitUser.findGitUser('anatawa12', octokit))
+    .resolves.toEqual([
+      'anatawa12',
+      '22656849+anatawa12@users.noreply.github.com',
+    ])
 })
 
 test('github actions account', async () => {
-  await expect(
-    gitUser.findGitUser('github-actions[bot]', octokit)
-  ).resolves.toEqual([
-    'github-actions[bot]',
-    '41898282+github-actions[bot]@users.noreply.github.com'
-  ])
+  await expect(gitUser.findGitUser('github-actions[bot]', octokit))
+    .resolves.toEqual([
+      'github-actions[bot]',
+      '41898282+github-actions[bot]@users.noreply.github.com',
+    ])
 })

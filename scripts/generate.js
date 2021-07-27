cb7974695a2120a6601c106820049d02a139b7ba
anatawa12 <anatawa12@icloud.com>
2021-07-27 16:02:51 +0900

    move generator

---
+++ scripts/generate.js
@@ -0,0 +1,23 @@
+/* eslint-disable eslint-comments/no-unlimited-disable */
+/* eslint-disable */
+const {readFileSync, writeFileSync, mkdirSync} = require('fs')
+const {join} = require('path')
+const yaml = require('js-yaml')
+const { compile } = require('json-schema-to-typescript')
+
+// compile from file
+
+// or, compile a JS object
+const mySchema = yaml.load(
+  readFileSync(join(__dirname, "..", "schema.yml"),
+    {encoding: "utf8"}))
+
+mkdirSync(join(__dirname, "..", "src", "generated"), {recursive: true})
+
+compile(mySchema, 'schema.yml')
+  .then(ts =>
+    writeFileSync(join(__dirname, "..", "src", "generated", "yaml.d.ts"), ts),
+  )
+
+writeFileSync(join(__dirname, "..", "src", "generated", "schema.json"), 
+  JSON.stringify(mySchema))

f4bbbe668b7368efb3f62fa6e4f598a8467c3878
anatawa12 <anatawa12@icloud.com>
2021-10-31 12:06:13 +0900

    small fix: exit with -1 if command exit with exception

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-export {main} from './command'
+import {main as commandMain} from './command'
+
+export function main(...args: string[]): void {
+  Promise.resolve()
+    .then(async () => commandMain(...args))
+    .catch(e => {
+      // eslint-disable-next-line no-console
+      console.error(e)
+      process.exit(-1)
+    })
+}

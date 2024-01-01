b83e79b6d397f995d07577a9eb43a1cb8eca0d1f
anatawa12 <anatawa12@icloud.com>
2024-01-01 17:18:16 +0900

    chore: fix sanitize_cmd

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,19 @@ async fn main() {
     exit(do_main(std::env::args()).await)
 }
 
+fn sanitize_cmd(mut cmd: &str) -> &str {
+    if cfg!(windows) {
+        cmd = cmd.strip_suffix(".exe").unwrap_or(cmd)
+    }
+    if let Some(slash) = cmd.rfind('/') {
+        cmd = &cmd[slash + 1..]
+    }
+    cmd
+}
+
 async fn do_main(mut args: Args) -> i32 {
     loop {
-        return match args.next().as_deref().map(|x| x.strip_suffix(".exe").unwrap_or(x)) {
+        return match args.next().as_deref().map(sanitize_cmd) {
             None => {
                 eprintln!("No command specified");
                 1

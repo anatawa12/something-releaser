88d0baa34f0abce4ebdf9e12df0bf1ec2455ac02
anatawa12 <anatawa12@icloud.com>
2024-01-05 04:01:38 +0900

    chore: remove javascript implementation entirely

--- __tests__/files/groovy.test.ts
+++
@@ -1,7 +0,0 @@
-import {expect, test} from '@jest/globals'
-import {GroovyGenerator} from '../../src/files/groovy'
-
-test("string literal", () => {
-  expect(new GroovyGenerator().line("%s", "hello\nworld").toString())
-    .toBe('"hello\\nworld"\n')
-})

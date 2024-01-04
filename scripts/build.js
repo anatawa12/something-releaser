1a03e66a4f991ab72d57465165f94ddb4596e9d0
anatawa12 <anatawa12@icloud.com>
2024-01-05 03:19:08 +0900

    feat: build-on-install

--- scripts/build.js
+++ scripts/build.js
@@ -5,13 +5,20 @@ const { execFileSync } = require("child_process");
 const { mkdirSync, cpSync } = require("fs");
 const { detectTarget, targetExt, exeFolder, exePath} = require("./platform");
 
-const target = process.argv[2] || detectTarget();
+function buildForTarget(target) {
+	execFileSync("cargo", ["build", "--release", "--target", target], {stdio: "inherit"});
 
-execFileSync("cargo", ["build", "--release", "--target", target], { stdio: "inherit" });
+	const exec_dir = `target/${target}/release`;
+	const exec_ext = targetExt(target);
 
-const exec_dir = `target/${target}/release`;
-const exec_ext = targetExt(target);
+	mkdirSync(exeFolder(target), {recursive: true});
+	cpSync(`${exec_dir}/something-releaser${exec_ext}`, exePath(target));
+}
 
-mkdirSync(exeFolder(target), { recursive: true });
-cpSync(`${exec_dir}/something-releaser${exec_ext}`, exePath(target));
+module.exports = {
+	buildForTarget
+};
 
+if (require.main === module) {
+	buildForTarget(process.argv[2] || detectTarget());
+}
--- scripts/install.js
+++ scripts/install.js
@@ -6,38 +6,64 @@ const { detectTarget, exePath } = require("./platform");
 const { normalize } = require("path");
 const { mkdirSync, existsSync, cpSync, linkSync, chmodSync, symlinkSync} = require("node:fs");
 
-const target = process.env.INPUT_TARGET_NAME || detectTarget();
+function installForTarget(target) {
+	console.log(`installing for ${target}`);
 
-console.log(`installing for ${target}`);
+	const sourceExePath = exePath(target);
+	const pathFolder = normalize(__dirname + `/../paths/${target}`);
 
-const sourceExePath = exePath(target);
-const pathFolder = normalize(__dirname + `/../paths/${target}`);
+	if (existsSync(pathFolder)) {
+		console.log("Already installed");
+		process.exit(0);
+	}
 
-if (existsSync(pathFolder)) {
-	console.log("Already installed");
-	process.exit(0);
+	mkdirSync(pathFolder, {recursive: true});
+
+	const commands = execFileSync(sourceExePath, ["internal-list"], {encoding: "utf-8"}).split("\n").filter((line) => line !== "");
+
+	let installer
+	if (target.includes("windows")) {
+		console.log(`using copy to create multicall binaries`);
+		installer = cpSync
+	} else {
+		console.log(`using symlink to create multicall binaries`);
+		installer = (source, dst) => {
+			symlinkSync(source, dst);
+			chmodSync(dst, 0o755);
+		}
+	}
+
+	for (const command of commands) {
+		const path = `${pathFolder}/${command}`;
+		installer(sourceExePath, path)
+		console.log(`::debug::installed to ${path}`);
+	}
+
+	execFileSync(sourceExePath, ["gh-add-path", "--", pathFolder], {stdio: "inherit"})
 }
 
-mkdirSync(pathFolder, { recursive: true });
-
-const commands = execFileSync(sourceExePath, ["internal-list"], { encoding: "utf-8" }).split("\n").filter((line) => line !== "");
-
-let installer
-if (target.includes("windows")) {
-	console.log(`using copy to create multicall binaries`);
-	installer = cpSync
-} else {
-	console.log(`using symlink to create multicall binaries`);
-	installer = (source, dst) => {
-		symlinkSync(source, dst);
-		chmodSync(dst, 0o755);
+function toBoolean(value) {
+	if (typeof value !== "string") return !!value;
+	switch (value.toLowerCase()) {
+		case "true":
+		case "yes":
+		case "1":
+			return true;
+		case "false":
+		case "no":
+		case "0":
+		case null:
+			return false;
+		default:
+			return Boolean(value);
 	}
 }
 
-for (const command of commands) {
-	const path = `${pathFolder}/${command}`;
-	installer(sourceExePath, path)
-	console.log(`::debug::installed to ${path}`);
+if (require.main === module) {
+	const target = process.env["INPUT_TARGET"] || detectTarget();
+	const build = toBoolean(process.env["INPUT_BUILD-ON-INSTALL"]);
+	if (build) {
+		require("./build").buildForTarget(target);
+	}
+	installForTarget(target);
 }
-
-execFileSync(sourceExePath, ["gh-add-path", "--", pathFolder], { stdio: "inherit" })

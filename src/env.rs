a83d7e2bf5d68796c77473670c283f502306f222
anatawa12 <anatawa12@icloud.com>
2024-01-05 03:06:11 +0900

    feat: .something-releaser.toml support

--- src/env.rs
+++ src/env.rs
@@ -1,10 +1,11 @@
 use crate::version_changer::VersionChangers;
 use serde::Deserialize;
 use std::collections::HashMap;
+use log::debug;
 
 #[derive(Debug, Default, Deserialize)]
 pub(crate) struct ConfigFile {
-    #[serde(rename = "releaseChanger", default)]
+    #[serde(alias = "releaseChanger", default)]
     pub release_changer: Option<VersionChangers>,
     #[serde(rename = "target", default)]
     pub targets: HashMap<String, TargetConfig>,
@@ -12,15 +13,22 @@ pub(crate) struct ConfigFile {
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct TargetConfig {
-    #[serde(rename = "releaseChanger", default)]
+    #[serde(alias = "releaseChanger", default)]
     pub release_changer: Option<VersionChangers>,
 }
 
 pub(crate) async fn env_file() -> ConfigFile {
     if let Some(json) = read_or_none(".something-releaser.json").await {
+        debug!("parsing .something-releaser.json");
         return serde_json::from_str(&json).expect("parsing .something-releaser.json");
     }
 
+    if let Some(toml) = read_or_none(".something-releaser.toml").await {
+        debug!("parsing .something-releaser.toml");
+        return toml::from_str(&toml).expect("parsing .something-releaser.toml");
+    }
+
+    debug!("no config file found");
     return Default::default();
 
     async fn read_or_none(path: &str) -> Option<String> {
